a781eeac919629bf663fd74a42b9bb191f5429d9
Hugo Lima Romão <hugo8romao@gmail.com>
2020-11-25 11:25:35 -0400

    adicionando animação em links internos

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ initTabNav();
 // Accordion List
 function initAccordion() {
   const accordionList = document.querySelectorAll(".js-accordion dt");
-  
+
   if (accordionList.length) {
     accordionList[0].classList.add("ativo");
     accordionList[0].nextElementSibling.classList.add("ativo");
@@ -42,3 +42,29 @@ function initAccordion() {
 }
 
 initAccordion();
+
+// Links internos suaves
+function initScrollToSection() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    const SctionTopDistance = section.offsetTop;
+
+    window.scrollTo({
+      top: SctionTopDistance,
+      behavior: "smooth",
+    });
+
+    console.log(section);
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollToSection();
