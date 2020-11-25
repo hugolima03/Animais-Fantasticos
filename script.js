f6372a62789e09b4afc800d7d1d0906bb8226ba2
Hugo Lima Romão <hugo8romao@gmail.com>
2020-11-25 14:39:14 -0400

    animação de sections

--- script.js
+++ script.js
@@ -68,3 +68,28 @@ function initScrollToSection() {
 }
 
 initScrollToSection();
+
+// Animação de sections
+function initAnimaScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.7;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTopDistance = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTopDistance - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimaScroll();
