2d01dcc938d7c4b6adf424bff4a353b022b7ca2f
Hugo Lima Romão <hugo8romao@gmail.com>
2020-12-14 16:01:23 -0400

    adicionando efeito tooltip na seção Contato

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ import initScrollToSection from './modules/scroll-suave.js';
 import initAnimaScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initToolTip from './modules/tooltip.js';
 
 initScrollToSection();
 initAnimaScroll();
 initAccordion();
 initTabNav();
+initModal();
+initToolTip();
