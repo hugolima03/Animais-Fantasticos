ad7c5f496ac6d1ad543114e347aeeaaad3b8c322
Hugo Lima Romão <hugo8romao@gmail.com>
2020-12-14 16:52:04 -0400

    fazendo o efeito de dropdown menu com JS

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initToolTip from './modules/tooltip.js';
+import initDropDownMenu from './modules/dropdown-menu.js';
 
 initScrollToSection();
 initAnimaScroll();
