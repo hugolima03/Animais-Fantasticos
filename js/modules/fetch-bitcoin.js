cf9c2a4b90253c2d5181d14bcd88724b6bc91b01
Hugo Lima Romão <hugo8romao@gmail.com>
2021-01-08 18:37:07 -0400

    fetchBitcoin

---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,17 @@
+export default function initFetchBitcoin() {
+  const url = 'https://blockchain.info/ticker';
+  
+  async function fetchBitcoin(url) {
+    try {
+      const responseBitcoin = await fetch(url);
+      const responseBitcoinJson = await responseBitcoin.json();
+    
+      const btcPreco = document.querySelector('.btc-preco');
+      btcPreco.innerText = (1000 / responseBitcoinJson.BRL.sell).toFixed(4);
+    } catch(erro) {
+      console.log(erro);
+    }
+  }
+  
+  fetchBitcoin(url);
+}
