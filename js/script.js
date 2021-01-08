44fe54e42597ad082e0ed1caedc2d3740a5dee00
Hugo Lima Romão <hugo8romao@gmail.com>
2021-01-08 18:21:22 -0400

    animais fetch

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ import initModal from './modules/modal.js';
 import initToolTip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
 
 initScrollToSection();
 initAnimaScroll();
@@ -17,5 +17,5 @@ initModal();
 initToolTip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
