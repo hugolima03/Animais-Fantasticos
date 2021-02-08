4492026a298af15086789bada989a78564f15b99
Hugo Lima Romão <hugo8romao@gmail.com>
2021-02-08 10:54:33 -0300

    instalando ESLint

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,18 +1,18 @@
 // Accordion List
 export default function initAccordion() {
-  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+
+  function activeAccordion() {
+    this.classList.toggle('ativo');
+    this.nextElementSibling.classList.toggle('ativo');
+  }
 
   if (accordionList.length) {
-    accordionList[0].classList.add("ativo");
-    accordionList[0].nextElementSibling.classList.add("ativo");
-
-    function activeAccordion(event) {
-      this.classList.toggle("ativo");
-      this.nextElementSibling.classList.toggle("ativo");
-    }
+    accordionList[0].classList.add('ativo');
+    accordionList[0].nextElementSibling.classList.add('ativo');
 
     accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
+      item.addEventListener('click', activeAccordion);
     });
   }
 }
