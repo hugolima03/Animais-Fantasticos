2c10c38b5d2f345eea5397d138d3b68755852b63
Hugo Lima Romão <hugo8romao@gmail.com>
2021-02-18 11:29:24 -0400

    reafatorando animascroll

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
+import ScrollAnima from './modules/scroll-anima.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
@@ -25,7 +25,9 @@ modal.init();
 const toolTip = new Tooltip('[data-tooltip]', 'ativo');
 toolTip.init();
 
-initAnimacaoScroll();
+const scrollAnima = new ScrollAnima('');
+scrollAnima.init();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
